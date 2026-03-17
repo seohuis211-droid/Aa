ee781dc95ce3ca61524839c1272c700f8580fe49
seohuis211-droid <seohuis211@gmail.com>
2026-03-18 01:09:50 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,12 @@
+var myPageDatabase = {
+    "/pages/bangidong-yuheungjujeom": "방이동유흥주점1",
+    "/pages/bangidongyuheungs": "방이동유흥4",
+    "/pages/bangidong-yuheungs": "방이동유흥3",
+    "/pages/bangidongyuheung": "방이동유흥2",
+    "/pages/bangidong-yuheung": "방이동유흥1",
+    "/pages/bangidongnoraeppas": "방이동노래빠4",
+    "/pages/bangidong-noraeppas": "방이동노래빠3",
+    "/pages/bangidongnoraeppa": "방이동노래ppa2",
+    "/pages/bangidong-noraeppa": "방이동노래빠1",
+    "/pages/bangidongjjomos": "방이동쩜오4"
+};
